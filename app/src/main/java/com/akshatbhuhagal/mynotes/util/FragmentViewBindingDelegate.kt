004cb95771aabbdc2f4d5108cce1441a5ef96bc3
Akshat Bhuhagal <akshatbhuhagal@gmail.com>
2023-11-24 02:32:15 +0530

    viewLifecycleOwnerLiveData instead of viewLifecycleOwner.lifecycleScope to allow accessing binding in DestroyView

--- app/src/main/java/com/akshatbhuhagal/mynotes/util/FragmentViewBindingDelegate.kt
+++ app/src/main/java/com/akshatbhuhagal/mynotes/util/FragmentViewBindingDelegate.kt
@@ -1,62 +1,88 @@
 package com.akshatbhuhagal.mynotes.util
 
+import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
+    liveData.observe(this, Observer { it?.let { t -> action(t) } })
+}
+
 class FragmentViewBindingDelegate<T : ViewBinding>(
     val fragment: Fragment,
-    val viewBindingFactory: (View) -> T
+    val viewBindingFactory: (View) -> T,
+    val cleanUp: ((T?) -> Unit)?,
 ) : ReadOnlyProperty<Fragment, T> {
+
+    // A backing property to hold our value
     private var binding: T? = null
 
     init {
         fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
-            val viewLifecycleOwnerLiveDataObserver =
-                Observer<LifecycleOwner?> {
-                    val viewLifecycleOwner = it ?: return@Observer
-
-                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-                        override fun onDestroy(owner: LifecycleOwner) {
-                            binding = null
-                        }
-                    })
+            val viewLifecycleOwnerObserver = Observer<LifecycleOwner?> { owner ->
+                if (owner == null) {
+                    cleanUp?.invoke(binding)
+                    binding = null
                 }
+            }
 
             override fun onCreate(owner: LifecycleOwner) {
                 fragment.viewLifecycleOwnerLiveData.observeForever(
-                    viewLifecycleOwnerLiveDataObserver
+                    viewLifecycleOwnerObserver
                 )
             }
 
             override fun onDestroy(owner: LifecycleOwner) {
                 fragment.viewLifecycleOwnerLiveData.removeObserver(
-                    viewLifecycleOwnerLiveDataObserver
+                    viewLifecycleOwnerObserver
                 )
             }
         })
     }
 
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+    override fun getValue(
+        thisRef: Fragment,
+        property: KProperty<*>,
+    ): T {
         val binding = binding
-        if (binding != null) {
+        if (binding != null && binding.root === thisRef.view) {
             return binding
         }
 
-        val lifecycle = fragment.viewLifecycleOwner.lifecycle
-        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
-            throw IllegalStateException("Should not attempt to get bindings when Fragment views are destroyed.")
-        }
+        val view = thisRef.view
+            ?: throw IllegalStateException("Should not attempt to get bindings when Fragment's view is null.")
 
-        return viewBindingFactory(thisRef.requireView()).also { this.binding = it }
+        return viewBindingFactory(view).also { this.binding = it }
     }
 }
 
-fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
-    FragmentViewBindingDelegate(this, viewBindingFactory)
+inline fun <T : ViewBinding> Fragment.viewBinding(
+    crossinline viewBindingFactory: (View) -> T,
+    noinline cleanUp: ((T?) -> Unit)? = null,
+): FragmentViewBindingDelegate<T> =
+    FragmentViewBindingDelegate(this, { v -> viewBindingFactory(v) }, cleanUp)
+
+inline fun <T : ViewBinding> Fragment.viewInflateBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T,
+    noinline cleanUp: ((T?) -> Unit)? = null,
+): FragmentViewBindingDelegate<T> =
+    FragmentViewBindingDelegate(
+        fragment = this,
+        viewBindingFactory = { v -> bindingInflater(LayoutInflater.from(v.context)) },
+        cleanUp = cleanUp
+    )
+
+inline fun <T : ViewBinding> AppCompatActivity.viewInflateBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T,
+) =
+    lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
+    }
